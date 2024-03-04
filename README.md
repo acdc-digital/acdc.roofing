6f4f4c182dcf3cb4e68b1143b9df996244824a65
Matthew Simon <elvatolighting@gmail.com>
2024-03-04 13:50:54 -0400

    Initialize Project with Clerk and ShadCN Step 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ACDC.Roofing
 ACDC.Roofing is our Client Repository for roofing related Proposals and Services. 
+<br>
+1. Added Clerk & ShadCN Initialization
+<br>
+2. 
---
+++ convex.roofing/convex/auth.config.ts
@@ -0,0 +1,10 @@
+// Clerk: https://suited-mule-68.clerk.accounts.dev
+
+export default {
+	providers: [
+	  {
+		domain: "https://suited-mule-68.clerk.accounts.dev",
+		applicationID: "convex",
+	  },
+	]
+  };
