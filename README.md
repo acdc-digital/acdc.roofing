4401a35bafe01778069170ac3f31f8a66e96063e
Matthew Simon <elvatolighting@gmail.com>
2024-03-04 14:08:35 -0400

    Initialize Project Convex Mutation Step 2

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ ACDC.Roofing is our Client Repository for roofing related Proposals and Services
 <br>
 1. Added Clerk & ShadCN Initialization
 <br>
-2. 
+2. First Convex Query (issue #001)
--- convex.roofing/src/app/page.tsx
+++ convex.roofing/src/app/page.tsx
@@ -7,8 +7,11 @@ import {
   SignedIn, 
   SignedOut,
  } from "@clerk/nextjs"; 
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const createFile = useMutation(api.files.createFile);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -23,6 +26,16 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      <Button 
+        onClick={() => {
+        createFile({
+          name: "hello world", 
+        });
+      }}
+      >
+        Click Me
+      </Button>
     </main>
   );
 }
