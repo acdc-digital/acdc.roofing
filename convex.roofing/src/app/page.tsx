4bdb9496506f9705ca11f6dae08f49baf1e58556
Matthew Simon <elvatolighting@gmail.com>
2024-03-06 11:59:49 -0400

    Updated Styling for File Grid Layout

--- convex.roofing/src/app/page.tsx
+++ convex.roofing/src/app/page.tsx
@@ -7,6 +7,7 @@ import {
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { UploadButton } from "./upload-button";
+import { Filecard } from "./file-card";
 
 export default function Home() {
   const organization = useOrganization();
@@ -21,14 +22,16 @@ export default function Home() {
 
   return (
     <main className="container mx-auto pt-12">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center mb-8">
       <h1 className="text-4xl font-bold">Your Files</h1>
       <UploadButton />
       </div>
-      {files?.map((file) => {
-        return <div key={file._id}>{file.name}</div>;
-      })}
 
+      <div className="grid grid-cols-4 gap-4">
+      {files?.map((file) => {
+        return <Filecard key={file._id} file={file} />
+      })}
+      </div>
     </main>
   );
 }
