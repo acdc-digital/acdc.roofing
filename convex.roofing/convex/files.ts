1ad5226a4d9cf76c16765f7d137b6363e1af3870
Matthew Simon <elvatolighting@gmail.com>
2024-03-05 21:24:08 -0400

    Completed OrgId Authorization

--- convex.roofing/convex/files.ts
+++ convex.roofing/convex/files.ts
@@ -1,5 +1,19 @@
 import { ConvexError, v } from "convex/values";
-import { mutation, query } from "./_generated/server"; 
+import { MutationCtx, QueryCtx, mutation, query } from "./_generated/server"; 
+import { getUser } from "./users";
+
+async function hasAccessToOrg(ctx: QueryCtx | MutationCtx,
+	tokenIdentifier: string, 
+	orgId: string
+	) {
+	const user = await getUser(ctx, tokenIdentifier);
+
+	const hasAccess = 
+	user.orgIds.includes(orgId) || 
+	user.tokenIdentifier.includes(orgId);
+
+		return hasAccess; 
+}
 
 export const createFile = mutation({
 	args: {
@@ -11,6 +25,16 @@ export const createFile = mutation({
 		
 		if (!identity) {
 			throw new ConvexError('You must be logged in to upload a file');
+		}
+
+		const hasAccess = await hasAccessToOrg(
+			ctx, 
+			identity.tokenIdentifier, 
+			args.orgId
+			);
+
+		if (!hasAccess) {
+			throw new ConvexError("You do not have access to this org");
 		}
 
 		await ctx.db.insert("files", {
@@ -30,6 +54,16 @@ export const getFiles = query({
 		if (!identity) {
 			return [];
 		}
+
+		const hasAccess = await hasAccessToOrg(
+			ctx, 
+			identity.tokenIdentifier, 
+			args.orgId
+			);
+
+			if (!hasAccess) {
+				return [];
+			}
 
 		return ctx.db
 			.query("files")
--- convex.roofing/convex/users.ts
+++ convex.roofing/convex/users.ts
@@ -1,5 +1,23 @@
 import { ConvexError, v } from "convex/values";
-import { internalMutation } from "./_generated/server";
+import { MutationCtx, QueryCtx, internalMutation } from "./_generated/server";
+
+export async function getUSer(
+	ctx: QueryCtx | MutationCtx,
+	tokenIdentifier: string
+	) {
+	const user = await ctx.db
+	.query("users")
+	.withIndex("by_tokenIdentifier", (q) =>
+		q.eq.("tokenIdentifier", identity.tokenIdentifier) 
+	)
+	.first();
+
+	if (!user) {
+		throw new ConvexError("Expected user to be defined");
+	}
+
+	return user;
+}
 
 export const createUser = internalMutation({
 	args: { tokenIdentifier: v.string() },
@@ -14,15 +32,7 @@ export const createUser = internalMutation({
 export const addOrgIdToUser = internalMutation({
 	args: { tokenIdentifier: v.string(), orgId: v.string() },
 	async handler(ctx, args) {
-		const user = await ctx.db
-		.query("users")
-		.withIndex('by_tokenIdentifier', (q) =>
-		q.eq("tokenIdentifier", args.tokenIdentifier)
-		).first()
-
-		if (!user) {
-			throw new ConvexError("Expected user to be defined");
-		}
+		const user = await getUSer(ctx, args.tokenIdentifier);
 
 		await ctx.db.patch(user._id, {
 			orgIds: [...user.orgIds, args.orgId],
